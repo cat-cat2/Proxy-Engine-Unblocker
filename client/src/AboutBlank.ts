50435074cfd11d1728efb1af862e794b3fd41007
catttttttt <ameertaimoorahmad@gmail.com>
2026-02-26 18:26:09 -0500

    fonts

--- client/src/AboutBlank.ts
+++ client/src/AboutBlank.ts
@@ -1,6 +1,6 @@
 export function Cloak() {
   const win = window.open();
-  const url = "https://mj89sp3sau2k7lj1eg3k40hkeppguj6j-a-sites-opensocial.googleusercontent.com/...";
+  const url = "http://localhost:5173";
   if (win && url) {
     const iframe = win.document.createElement("iframe");
     iframe.style.border = "none";
