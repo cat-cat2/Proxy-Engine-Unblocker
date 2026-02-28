ed08b3cabaa92415e52eb3765be4c3dd1e25793d
catttttttt <ameertaimoorahmad@gmail.com>
2026-02-28 09:25:06 -0500

    this

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,26 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+//import { useState, useEffect } from 'react';
+//import axios from 'axios';
 import './App.css';
-import './scripts/AboutBlank.ts';
-import './components/Credits_label.tsx';
-import './components/Version_Label.tsx';
-import { Version_Label } from './components/Version_Label.tsx';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+//import './scripts/AboutBlank.ts';
+//import './components/Credits_label.tsx';
+//import './components/Version_Label.tsx';
+//import { Version_Label } from './components/Version_Label.tsx';
+import { Home } from './pages/home.tsx';
+import { Settings } from './pages/settings.tsx';
+import { Games } from './pages/games.tsx';
+import { Chatbot } from './pages/chatbot.tsx';
 
 function App() {
   return (
-    <Version_Label />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/games" element={<Games />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+      </Routes>
+    </Router>
   );
 }
 
